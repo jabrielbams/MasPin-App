7910a6570bbcdf4fd438583592b9ba474850133f
Sheren Aura Vi <sherenaura21@gmail.com>
2024-01-07 15:16:10 +0700

    feat: create bottom-navigation

--- App.js
+++ App.js
@@ -6,35 +6,15 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
 // Import screens
-
 import LoginScreen from './src/screens/login';
 import RegisterScreen from './src/screens/register';
 import HomeScreen from './src/screens/home';
+import MainComponent from './src/navigation/MainComponent';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <MainComponent />;
 };
 
 export default App;
---
+++ src/screens/report/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import styles from './styles';
+
+const ReportScreen = () => {
+  return (
+    <View style={styles.mainBody}>
+      <Text>ReportScreen</Text>
+    </View>
+  );
+};
+
+export default ReportScreen;
